0ac24a29468ad0070ba6f7edc0fe77953384edea
James Arruda <james.arruda@gtri.gatech.edu>
2024-07-15 20:52:59 -0400

    More link updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,7 +74,7 @@ html_theme_options = {
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/gtri/upstage/",
+            "url": "https://github.com/JamesArruda/upstage/",
             "icon": "fa-brands fa-square-github",
             "type": "fontawesome",
         },
